f3ac27f5922186e89f524f7fb8625e73d20dea26
Šimun <simun.hordov5@gmail.com>
2023-11-06 22:02:52 +0100

    Add files via upload
    Layout for calendar improved

--- App.tsx
+++ App.tsx
@@ -1,8 +1,36 @@
+import React, { useState } from "react";
 import "./App.css";
-import { DatePicker } from "./components/DatePicker";
+import Calendar from "./component/Calendar";
 
 function App() {
-  return <DatePicker />;
+  // Start with the current month and year or any default you find suitable
+  const [month, setMonth] = useState(new Date().getMonth() + 1); // Months are 0-indexed in JavaScript Date
+
+  // Month names array to convert month number to month name
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  // The year is fixed as per your Calendar component
+  const year = 2023;
+
+  return (
+    <div>
+      <h1>{`${monthNames[month - 1]} ${year}`}</h1>
+      <Calendar tableId="MyTable" month={month} setMonth={setMonth} />
+    </div>
+  );
 }
 
 export default App;
--- Calendar.tsx
+++ Calendar.tsx
@@ -1,18 +1,90 @@
-import React from "react";
-import { Weekday, Date } from "../../types";
-import { Weekdays } from "../configs/weekdays";
+// Calendar.tsx
+import React, { useEffect } from "react";
+import "../App.css";
 
-export const Calendar: React.FC<{}> = ({}) => {
+function Calendar({ tableId, month, setMonth }) {
+  const year = 2023; // Fixed year
+
+  useEffect(() => {
+    // Ensure you've got a valid table element to work with
+    const tableBody = document.querySelector(`#${tableId} tbody`);
+    if (!tableBody) {
+      console.error(`Table body for table '${tableId}' not found.`);
+      return;
+    }
+
+    // Clear previous calendar rows
+    while (tableBody.firstChild) {
+      tableBody.removeChild(tableBody.firstChild);
+    }
+
+    // Calculate the first day of the month and the number of days in the month
+    const firstDayOfMonth = new Date(year, month - 1, 1).getDay();
+    const daysInMonth = new Date(year, month, 0).getDate();
+
+    let date = 1; // Calendar starts at the first day of the month
+
+    // Generate the calendar for a month
+    for (let row = 0; row < 6; row++) {
+      const weekRow = document.createElement("tr");
+
+      for (let cell = 0; cell < 7; cell++) {
+        if ((row === 0 && cell < firstDayOfMonth) || date > daysInMonth) {
+          weekRow.appendChild(document.createElement("td")); // Empty cell for alignment
+        } else {
+          const dateCell = document.createElement("td");
+          dateCell.textContent = date.toString();
+          weekRow.appendChild(dateCell);
+          date++; // Move to the next date
+        }
+      }
+
+      // Add the current row to the table
+      tableBody.appendChild(weekRow);
+
+      // Stop creating rows if we've run out of dates
+      if (date > daysInMonth) {
+        break;
+      }
+    }
+  }, [month, tableId]);
+
+  // Handlers to navigate months
+  const goToPreviousMonth = () => {
+    setMonth(month > 1 ? month - 1 : 12);
+  };
+
+  const goToNextMonth = () => {
+    setMonth(month < 12 ? month + 1 : 1);
+  };
+
   return (
-    <div className="calendar-container">
-      <div className="datpicker-container">
-        <div className="weekdays-container">
-          {Weekdays.map((day) => (
-            <div className="week-day">{day}</div>
-          ))}
-        </div>
-        <div className="calendar"></div>
+    <div className="calendar">
+      <div className="calendar-navigation">
+        <button onClick={goToPreviousMonth} disabled={month === 1}>
+          &lt; Prev
+        </button>
+        <span>{`${year}-${month.toString().padStart(2, "0")}`}</span>
+        <button onClick={goToNextMonth} disabled={month === 12}>
+          Next &gt;
+        </button>
       </div>
+      <table id={tableId}>
+        <thead>
+          <tr>
+            <th>Sunday</th>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednesday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
     </div>
   );
-};
+}
+
+export default Calendar;
