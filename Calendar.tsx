891663c931b1bcca45d780e1bfc9f36f25223d61
Šimun <simun.hordov5@gmail.com>
2023-11-06 22:31:12 +0100

    Add files via upload
    Fetching data from API in progress

--- Calendar.tsx
+++ Calendar.tsx
@@ -1,61 +1,101 @@
 // Calendar.tsx
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "../App.css";
 
 interface CalendarProps {
   tableId: string;
   month: number;
-  setMonth: (month: number) => void; // setMonth is a function that takes a number and returns void
+  setMonth: (month: number) => void;
 }
 
-function Calendar({ tableId, month, setMonth }: CalendarProps) {
+const Calendar: React.FC<CalendarProps> = ({ tableId, month, setMonth }) => {
   const year = 2023; // Fixed year
+  const [commitData, setCommitData] = useState<any[]>([]);
 
   useEffect(() => {
-    // Ensure you've got a valid table element to work with
+    // Function to fetch commit data
+    const fetchCommits = async () => {
+      // Construct the API URL for fetching commits for the given month and year
+      const since = new Date(year, month - 1, 1).toISOString();
+      const until = new Date(year, month, 0).toISOString();
+
+      try {
+        const response = await axios.get(
+          `https://api.github.com/repos/facebook/react/commits`,
+          {
+            params: {
+              since: since,
+              until: until,
+            },
+          }
+        );
+        setCommitData(response.data);
+      } catch (error) {
+        console.error("Error fetching commit data:", error);
+      }
+    };
+
+    fetchCommits();
+  }, [month]);
+
+  useEffect(() => {
     const tableBody = document.querySelector(`#${tableId} tbody`);
     if (!tableBody) {
       console.error(`Table body for table '${tableId}' not found.`);
       return;
     }
 
-    // Clear previous calendar rows
     while (tableBody.firstChild) {
       tableBody.removeChild(tableBody.firstChild);
     }
 
-    // Calculate the first day of the month and the number of days in the month
     const firstDayOfMonth = new Date(year, month - 1, 1).getDay();
     const daysInMonth = new Date(year, month, 0).getDate();
 
-    let date = 1; // Calendar starts at the first day of the month
-
-    // Generate the calendar for a month
+    let date = 1;
     for (let row = 0; row < 6; row++) {
       const weekRow = document.createElement("tr");
 
       for (let cell = 0; cell < 7; cell++) {
-        if ((row === 0 && cell < firstDayOfMonth) || date > daysInMonth) {
-          weekRow.appendChild(document.createElement("td")); // Empty cell for alignment
-        } else {
-          const dateCell = document.createElement("td");
-          dateCell.textContent = date.toString();
+        const dateCell = document.createElement("td");
+        if (row === 0 && cell < firstDayOfMonth) {
           weekRow.appendChild(dateCell);
-          date++; // Move to the next date
+        } else if (date > daysInMonth) {
+          weekRow.appendChild(dateCell);
+          continue;
+        } else {
+          // Check if there's commit data for this date and display it
+          const currentDate = new Date(year, month - 1, date);
+          const commitInfo = commitData.filter(
+            (commit) =>
+              new Date(commit.commit.committer.date).toDateString() ===
+              currentDate.toDateString()
+          );
+
+          dateCell.textContent = date.toString();
+          if (commitInfo.length > 0) {
+            // If there are commits on this date, add an indicator
+            const commitIndicator = document.createElement("div");
+            commitIndicator.className = "commit-indicator";
+            commitIndicator.title = commitInfo
+              .map((commit) => commit.commit.message)
+              .join("\n");
+            commitIndicator.textContent = "C"; // Placeholder for an icon or commit count
+            dateCell.appendChild(commitIndicator);
+          }
+
+          weekRow.appendChild(dateCell);
+          date++;
         }
       }
-
-      // Add the current row to the table
       tableBody.appendChild(weekRow);
-
-      // Stop creating rows if we've run out of dates
       if (date > daysInMonth) {
         break;
       }
     }
-  }, [month, tableId]);
+  }, [commitData, month, tableId]);
 
-  // Handlers to navigate months
   const goToPreviousMonth = () => {
     setMonth(month > 1 ? month - 1 : 12);
   };
@@ -91,6 +131,6 @@ function Calendar({ tableId, month, setMonth }: CalendarProps) {
       </table>
     </div>
   );
-}
+};
 
 export default Calendar;
